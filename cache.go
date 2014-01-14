230fee4ab48a7529cfb5d3f033fa8c7a48d1a1cf
feng <shenedu@gmail.com>
2014-01-14 15:40:28 +0800

    cache backedn by leveldb

--- cache.go
+++ cache.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"net/http"
 	"encoding/binary"
+	"github.com/jmhodges/levigo"
+	"net/http"
 	"time"
 )
 
@@ -64,7 +65,7 @@ func (e *CacheEntry) Decode(buffer []byte) {
 
 	bodylen, c := binary.Varint(buffer[n:])
 	n += c
-	e.Body = buffer[n:n + int(bodylen)]
+	e.Body = buffer[n : n+int(bodylen)]
 	n += int(bodylen)
 
 	headercnt, c := binary.Varint(buffer[n:])
@@ -73,7 +74,7 @@ func (e *CacheEntry) Decode(buffer []byte) {
 	for i := 0; i < int(headercnt); i++ {
 		keylen, c := binary.Varint(buffer[n:])
 		n += c
-		key := string(buffer[n:n + int(keylen)])
+		key := string(buffer[n : n+int(keylen)])
 		n += int(keylen)
 
 		valcnt, c := binary.Varint(buffer[n:])
@@ -81,36 +82,99 @@ func (e *CacheEntry) Decode(buffer []byte) {
 		for j := 0; j < int(valcnt); j++ {
 			vallen, c := binary.Varint(buffer[n:])
 			n += c
-			e.Header[key] = append(e.Header[key], string(buffer[n:n + int(vallen)]))
+			e.Header[key] = append(e.Header[key], string(buffer[n:n+int(vallen)]))
 			n += int(vallen)
 		}
 	}
 }
 
 type Cache interface {
-	Get(url string) (CacheEntry, bool)
-	Set(url string, entry CacheEntry)
+	Get(key string) (CacheEntry, bool)
+	Set(key string, val CacheEntry) error
+	Delete(key string) error
+	Close() error
 }
 
+// cache backend by RAM
 type MemCache map[string]CacheEntry
 
-func NewMemCache() MemCache {
-	return make(map[string]CacheEntry)
-}
+func NewMemCache() MemCache { return make(map[string]CacheEntry) }
 
-func (c MemCache) Get(url string) (CacheEntry, bool) {
+func (c MemCache) Close() error                     { return nil }
+func (c MemCache) Delete(key string)                { delete(c, key) }
+func (c MemCache) Set(key string, value CacheEntry) { c[key] = value }
+func (c MemCache) Get(key string) (CacheEntry, bool) {
 	t := time.Now().Unix()
-	e, ok := c[url];
+	e, ok := c[key]
 	if ok {
-		if e.AddTime + e.Expire < t {
+		if e.AddTime+e.Expire < t {
 			return e, ok
 		}
-		delete(c, url)
+		delete(c, key)
 	}
 	return e, false
 }
 
-func (c MemCache) Set(url string, entry CacheEntry) {
-	c[url] = entry
+// cache backend by leveldb(github.com/jmhodges/levigo)
+type LeveldbCache struct {
+	db    *levigo.DB
+	fp    *levigo.FilterPolicy
+	cache *levigo.Cache
+
+	Wo *levigo.WriteOptions
+	Ro *levigo.ReadOptions
+	So *levigo.ReadOptions
 }
 
+func NewLeveldbCache(dbname string, cacheM int) (*LeveldbCache, error) {
+	opts := levigo.NewOptions()
+	filter := levigo.NewBloomFilter(10)
+	cache := levigo.NewLRUCache(1024 * 1024 * cacheM)
+	opts.SetFilterPolicy(filter)
+	opts.SetCache(cache)
+	opts.SetCreateIfMissing(true)
+	opts.SetWriteBufferSize(8 * 1024 * 104) // 8M
+	opts.SetCompression(levigo.SnappyCompression)
+
+	if ldb, err := levigo.Open(dbname, opts); err == nil {
+		so := levigo.NewReadOptions()
+		so.SetFillCache(false)
+		return &LeveldbCache{
+			db:    ldb,
+			fp:    filter,
+			cache: cache,
+			Ro:    levigo.NewReadOptions(),
+			Wo:    levigo.NewWriteOptions(),
+			So:    so,
+		}, nil
+	} else {
+		return nil, err
+	}
+}
+
+func (c *LeveldbCache) Close() error {
+	c.Wo.Close()
+	c.Ro.Close()
+	c.So.Close()
+	c.db.Close()
+	c.fp.Close()
+	c.cache.Close()
+	return nil
+}
+
+func (c *LeveldbCache) Set(key string, val CacheEntry) error {
+	return c.db.Put(c.Wo, []byte(key), val.Encode())
+}
+
+func (c *LeveldbCache) Get(key string) (CacheEntry, bool) {
+	e := CacheEntry{}
+	if d, err := c.db.Get(c.Ro, []byte(key)); err == nil && len(d) > 0 {
+		e.Decode(d)
+		return e, true
+	}
+	return e, false
+}
+
+func (c *LeveldbCache) Delete(key string) error {
+	return c.db.Delete(c.Wo, []byte(key))
+}
--- cache_test.go
+++ cache_test.go
@@ -1,17 +1,25 @@
 package main
 
 import (
-	"testing"
-	"encoding/gob"
 	"bytes"
+	"code.google.com/p/leveldb-go/leveldb"
+	"code.google.com/p/leveldb-go/leveldb/db"
+	"encoding/gob"
+	"github.com/jmhodges/levigo"
+	"log"
+	"os"
 	"reflect"
+	"strconv"
+	"testing"
 )
 
+// CGO_CFLAGS="-I/path/to/leveldb/include" CGO_LDFLAGS="-L/path/to/leveldb/lib" go get github.com/jmhodges/levigo
+
 var entry = CacheEntry{
-	Status: 200,
+	Status:  200,
 	AddTime: 100000010,
-	Expire: 100101,
-	Header:  map[string][]string{
+	Expire:  100101,
+	Header: map[string][]string{
 		"X-header": []string{"abc", "def"},
 	},
 	Body: []byte("hello world"),
@@ -26,7 +34,7 @@ func TestEncoding(t *testing.T) {
 	}
 }
 
-func TestMemTable(t *testing.T) {
+func TestMemCache(t *testing.T) {
 	cache := NewMemCache()
 	url := "http://test.com"
 	cache.Set(url, entry)
@@ -36,6 +44,44 @@ func TestMemTable(t *testing.T) {
 	} else {
 		t.Fail()
 	}
+}
+
+func TestLeveldbCache(t *testing.T) {
+	path := "/tmp/leveldb_cache__"
+	os.RemoveAll(path)
+	cache, err := NewLeveldbCache(path, 10)
+	if err != nil {
+		t.Fail()
+	}
+	url := "http://test.com"
+	cache.Set(url, entry)
+
+	if r, ok := cache.Get(url); ok && reflect.DeepEqual(r, entry) {
+
+	} else {
+		t.Fail()
+	}
+
+	cache.Delete(url)
+	if _, ok := cache.Get(url); ok {
+		t.Fail()
+	}
+	cache.Close()
+}
+
+func BenchmarkLeveldbCache(b *testing.B) {
+	path := "/tmp/leveldb_cache__"
+	os.RemoveAll(path)
+	cache, err := NewLeveldbCache(path, 10)
+	if err != nil {
+		b.Fail()
+	}
+
+	for i := 0; i < b.N; i++ {
+		url := "http://test.com" + strconv.Itoa(i)
+		cache.Set(url, entry)
+	}
+	cache.Close()
 }
 
 func BenchmarkEncoding(b *testing.B) {
@@ -54,14 +100,13 @@ func BenchmarkDecoding(b *testing.B) {
 	}
 }
 
-
 func BenchmarkGobDecoding(b *testing.B) {
 	var buffer bytes.Buffer
 	enc := gob.NewEncoder(&buffer)
 	enc.Encode(entry)
 	data := buffer.Bytes()
 
-	for i := 0 ; i < b.N; i++ {
+	for i := 0; i < b.N; i++ {
 		buffer := bytes.NewReader(data)
 		dec := gob.NewDecoder(buffer)
 		e := CacheEntry{}
@@ -70,9 +115,66 @@ func BenchmarkGobDecoding(b *testing.B) {
 	}
 }
 
+func TestLevigo(t *testing.T) {
+	path := "/tmp/levigo_test_10101"
+	os.RemoveAll(path)
+
+	opts := levigo.NewOptions()
+	filter := levigo.NewBloomFilter(10)
+	opts.SetFilterPolicy(filter)
+	opts.SetCache(levigo.NewLRUCache(1024 << 20)) // 1G
+	opts.SetCreateIfMissing(true)
+	if ldb, err := levigo.Open(path, opts); err == nil {
+		key := []byte("test-test hwl0dsfds")
+		val := []byte("value")
+
+		if err = ldb.Put(levigo.NewWriteOptions(), key, val); err != nil {
+			t.Fail()
+		} else {
+			ro := levigo.NewReadOptions()
+			if data, err := ldb.Get(ro, key); err == nil && reflect.DeepEqual(data, val) {
+				ro.SetFillCache(false)
+				it := ldb.NewIterator(ro)
+				it.Seek([]byte{0})
+				for ; it.Valid(); it.Next() {
+					log.Printf("%s => %s", it.Key(), it.Value())
+				}
+			} else {
+				t.Fail()
+			}
+		}
+	} else {
+		t.Fail()
+	}
+}
+
+func TestLevelDB(t *testing.T) {
+	path := "/tmp/leveldb_test_10101"
+	os.RemoveAll(path)
+
+	options := db.Options{}
+	if ldb, err := leveldb.Open(path, &options); err == nil {
+		key := []byte("test-test hwl0dsfds")
+		val := []byte("value")
+		ldb.Set(key, val, &db.WriteOptions{})
+
+		if r, err := ldb.Get(key, &db.ReadOptions{}); err == nil && reflect.DeepEqual(r, val) {
+			// it := ldb.Find(nil, nil)
+			// for it.Next() {
+			// 	log.Printf("%s => %s", it.Key(), it.Value())
+			// }
+
+		} else {
+			t.Fail()
+		}
+	} else {
+		t.Fail()
+	}
+
+}
 
 func BenchmarkGobEncoding(b *testing.B) {
-	for i := 0 ; i < b.N; i++ {
+	for i := 0; i < b.N; i++ {
 		var buffer bytes.Buffer
 		enc := gob.NewEncoder(&buffer)
 		enc.Encode(entry)
